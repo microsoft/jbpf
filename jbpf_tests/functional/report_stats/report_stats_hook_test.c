fd9bca2c6079033e6a036eb12f4b8dc7a7f9a911
Zhihua Lai <zhihualai@microsoft.com>
2024-12-11 18:22:28 +0000

    Changed to a simple local test

--- jbpf_tests/functional/report_stats/report_stats_hook_test.c
+++ jbpf_tests/functional/report_stats/report_stats_hook_test.c
@@ -25,28 +25,8 @@ pid_t cpid = -1;
 
 sem_t sem;
 
-#define LCM_IPC_SEM_NAME "/jbpf_e2e_lcm_report_stats_ipc_standalone_sem"
-#define LCM_IPC_AGENT_SEM_NAME "/jbpf_e2e_report_stats_lcm_ipc_agent_sem"
-
-sem_t *lcm_ipc_sem, *lcm_ipc_agent_sem;
-
 jbpf_io_stream_id_t stream_id_c1 = {
     .id = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF}};
-
-void
-handle_sigchld(int sig)
-{
-    return;
-}
-
-void
-handle_sigterm(int sig)
-{
-    if (cpid > 0) {
-        kill(cpid, SIGKILL);
-    }
-    exit(EXIT_FAILURE);
-}
 
 static void
 io_channel_check_output(jbpf_io_stream_id_t* stream_id, void** bufs, int num_bufs, void* ctx)
@@ -72,13 +52,17 @@ io_channel_check_output(jbpf_io_stream_id_t* stream_id, void** bufs, int num_buf
 }
 
 int
-run_jbpf_agent(void)
+main(int argc, char** argv)
 {
-
+    struct jbpf_codeletset_load_req codeletset_req_c1 = {0};
+    struct jbpf_codeletset_unload_req codeletset_unload_req_c1 = {0};
+    const char* jbpf_path = getenv("JBPF_PATH");
     struct jbpf_config config = {0};
 
     jbpf_set_default_config_options(&config);
     sem_init(&sem, 0, 0);
+
+    config.lcm_ipc_config.has_lcm_ipc_thread = false;
 
     assert(jbpf_init(&config) == 0);
 
@@ -88,50 +72,7 @@ run_jbpf_agent(void)
     // Register a callback to handle the buffers sent from the codelets
     jbpf_register_io_output_cb(io_channel_check_output);
 
-    // Notify the LCM tool that we can now load the codeletset
-    sem_post(lcm_ipc_sem);
-
-    // Wait until the codeletset is loaded
-    sem_wait(lcm_ipc_agent_sem);
-
-    // Wait for all the output checks to finish
-    sem_wait(&sem);
-
-    // Test is done. Notify the LCM IPC tool that it can proceed with unloading the codelet
-    sem_post(lcm_ipc_sem);
-
-    // Wait for the LCM IPC tools to finish
-    sem_wait(lcm_ipc_agent_sem);
-
-    // Stop
-    jbpf_stop();
-
-    sem_destroy(&sem);
-
-    return 0;
-}
-
-int
-run_lcm_ipc_loader(void)
-{
-    struct jbpf_codeletset_load_req codeletset_req_c1 = {0};
-    struct jbpf_codeletset_unload_req codeletset_unload_req_c1 = {0};
-    jbpf_lcm_ipc_address_t address = {0};
-
-    const char* jbpf_path = getenv("JBPF_PATH");
-
-    snprintf(
-        address.path,
-        sizeof(address.path) - 1,
-        "%s/%s/%s",
-        JBPF_DEFAULT_RUN_PATH,
-        JBPF_DEFAULT_NAMESPACE,
-        JBPF_DEFAULT_LCM_SOCKET);
-
-    // Wait until the agent is initialized and is expecting incoming connections
-    sem_wait(lcm_ipc_sem);
-
-    // Make a request to load codeletset C1 in hook "test1"
+    // Load the codeletset with codelet C1 in hook "test1"
 
     // The name of the codeletset
     strcpy(codeletset_req_c1.codeletset_id.name, "simple_output_codeletset");
@@ -151,6 +92,7 @@ run_lcm_ipc_loader(void)
     codeletset_req_c1.codelet_descriptor[0].out_io_channel[0].has_serde = false;
     codeletset_req_c1.codelet_descriptor[0].num_linked_maps = 0;
 
+    // The path of the codelet
     assert(jbpf_path != NULL);
     snprintf(
         codeletset_req_c1.codelet_descriptor[0].codelet_path,
@@ -160,93 +102,21 @@ run_lcm_ipc_loader(void)
     strcpy(codeletset_req_c1.codelet_descriptor[0].codelet_name, "simple_output");
     strcpy(codeletset_req_c1.codelet_descriptor[0].hook_name, "report_stats");
 
-    // Make a request to load the codeletset
-    assert(jbpf_lcm_ipc_send_codeletset_load_req(&address, &codeletset_req_c1) == JBPF_LCM_IPC_REQ_SUCCESS);
+    // Load the codeletset
+    assert(jbpf_codeletset_load(&codeletset_req_c1, NULL) == JBPF_CODELET_LOAD_SUCCESS);
 
-    // Loading was done, so the agent test can move on
-    sem_post(lcm_ipc_agent_sem);
+    // Wait for all the output checks to finish
+    sem_wait(&sem);
 
-    // Wait for the test to finish
-    sem_wait(lcm_ipc_sem);
-
-    // Test is done. Let's unload the codeletset
+    // Unload the codeletsets
     strcpy(codeletset_unload_req_c1.codeletset_id.name, "simple_output_codeletset");
+    assert(jbpf_codeletset_unload(&codeletset_unload_req_c1, NULL) == JBPF_CODELET_UNLOAD_SUCCESS);
 
-    // Make a request to unload an existing codeletset
-    assert(jbpf_lcm_ipc_send_codeletset_unload_req(&address, &codeletset_unload_req_c1) == JBPF_LCM_IPC_REQ_SUCCESS);
+    // Stop
+    jbpf_stop();
 
-    // Tests are done. Let the agent know that it can terminate
-    sem_post(lcm_ipc_agent_sem);
-    return 0;
-}
+    sem_destroy(&sem);
 
-int
-main(int argc, char** argv)
-{
-
-    pid_t child_pid;
-    int secondary_status;
-    int res;
-
-    JBPF_UNUSED(res);
-
-    // Register some signals to kkill the test, if it fails
-    struct sigaction sa;
-    sa.sa_handler = &handle_sigchld;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-    if (sigaction(SIGCHLD, &sa, 0) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-
-    struct sigaction sa_child;
-    sa_child.sa_handler = &handle_sigterm;
-    sigemptyset(&sa_child.sa_mask);
-    sa_child.sa_flags = 0;
-    if (sigaction(SIGTERM, &sa_child, 0) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-    if (sigaction(SIGINT, &sa_child, 0) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-    if (sigaction(SIGABRT, &sa_child, 0) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-
-    // Remove the semaphore if the test did not finish gracefully
-    sem_unlink(LCM_IPC_SEM_NAME);
-    sem_unlink(LCM_IPC_AGENT_SEM_NAME);
-
-    lcm_ipc_sem = sem_open(LCM_IPC_SEM_NAME, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 0);
-    if (lcm_ipc_sem == SEM_FAILED) {
-        exit(EXIT_FAILURE);
-    }
-
-    lcm_ipc_agent_sem = sem_open(LCM_IPC_AGENT_SEM_NAME, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 0);
-    if (lcm_ipc_agent_sem == SEM_FAILED) {
-        exit(EXIT_FAILURE);
-    }
-
-    child_pid = fork();
-    assert(child_pid >= 0);
-
-    if (child_pid == 0) {
-        assert(run_lcm_ipc_loader() == 0);
-    } else {
-        cpid = child_pid; // TODO
-        assert(run_jbpf_agent() == 0);
-        res = wait(&secondary_status);
-        assert(res != -1);
-        assert(secondary_status == 0);
-
-        printf("Test is now complete...\n");
-    }
-
-    sem_unlink(LCM_IPC_SEM_NAME);
-    sem_unlink(LCM_IPC_AGENT_SEM_NAME);
+    printf("Test completed successfully.\n");
     return 0;
 }
