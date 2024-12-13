5acbd47064b78979e6bd3f8f4660ef75ccd82e17
Zhihua Lai <zhihualai@microsoft.com>
2024-12-13 15:24:49 +0000

    Continued work

--- jbpf_tests/concurrency/mem/mem_jbpf_io_channel_share_data_ptr_tests.c
+++ jbpf_tests/concurrency/mem/mem_jbpf_io_channel_share_data_ptr_tests.c
@@ -1,20 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 /*
-This test evaluates the behavior of the jbpf_io_channel_share_data_ptr API in a multithreaded environment.
-It is designed to simulate a typical producer-consumer-worker pattern:
-
-- **Producer Thread:** Produces structured test data and submits it to a channel.
-- **Consumer Thread:** Retrieves data from the channel, processes it briefly, and delegates processing tasks to worker
-threads.
-- **Worker Threads:** Perform parallel processing on shared data pointers obtained from the
-jbpf_io_channel_share_data_ptr function.
-
-Key Objectives:
-1. Validate the correctness and safety of shared data pointers in concurrent use.
-2. Ensure proper buffer management (reservation, submission, and release).
-3. Verify that worker threads can process shared data pointers concurrently without data corruption.
-4. Verify the correctness of the jbpf_io_channel_share_data_ptr API in a multithreaded environment.
-*/
+This test ensures each consumer thread sends data to a unique worker thread.*/
 
 #include <stdint.h>
 #include <stdio.h>
@@ -32,7 +18,7 @@ Key Objectives:
 #include "jbpf_io_channel.h"
 #include "jbpf_io_utils.h"
 
-#define NUM_WORKER_THREADS 4
+#define NUM_THREADS 4
 #define CHANNEL_NUMBER_OF_ELEMENTS 100
 #define CHANNEL_CAPACITY 255
 
@@ -46,18 +32,23 @@ struct jbpf_io_stream_id local_stream_id = {
     .id = {0xF5, 0xFF, 0XFF, 0XFF, 0xFF, 0xFF, 0XFF, 0XFF, 0xFF, 0xFF, 0XFF, 0XFF, 0xFF, 0xFF, 0XFF, 0XFF}};
 
 sem_t producer_sem;
-sem_t consumer_sem;
-sem_t worker_start_sem;
-sem_t worker_done_sem;
+sem_t consumer_sem[NUM_THREADS];
+sem_t worker_start_sem[NUM_THREADS];
+sem_t worker_done_sem[NUM_THREADS];
 
 jbpf_io_channel_t* local_channel;
-volatile int stop_workers = 0;
-struct test_struct* current_buffer = NULL;
+
+struct worker_thread_args
+{
+    struct test_struct** shared_buffer_ptr;
+    int thread_index;
+};
 
 void*
 producer_thread_func(void* arg)
 {
-    for (u_int32_t i = 0; i < CHANNEL_CAPACITY; i++) {
+    jbpf_io_register_thread();
+    for (uint32_t i = 0; i < CHANNEL_CAPACITY; i++) {
         sem_wait(&producer_sem);
 
         struct test_struct* buffer = jbpf_io_channel_reserve_buf(local_channel);
@@ -66,7 +57,7 @@ producer_thread_func(void* arg)
         buffer->value = i * 10;
 
         assert(jbpf_io_channel_submit_buf(local_channel) == 0);
-        sem_post(&consumer_sem);
+        sem_post(&consumer_sem[i % NUM_THREADS]);
     }
     return NULL;
 }
@@ -74,29 +65,31 @@ producer_thread_func(void* arg)
 void*
 consumer_thread_func(void* arg)
 {
-    for (u_int32_t i = 0; i < CHANNEL_CAPACITY; i++) {
-        sem_wait(&consumer_sem);
+    jbpf_io_register_thread();
+    struct worker_thread_args* args = (struct worker_thread_args*)arg;
+    int thread_index = args->thread_index;
 
-        void* recv_ptr[10];
-        int num_received = jbpf_io_channel_recv_data(local_channel, recv_ptr, 1);
-        assert(num_received == 1);
+    sem_wait(&consumer_sem[thread_index]);
 
-        current_buffer = (struct test_struct*)recv_ptr[0];
+    void* recv_ptr[10];
+    int num_received = jbpf_io_channel_recv_data(local_channel, recv_ptr, 1);
+    assert(num_received == 1);
 
-        // Signal workers to start processing
-        for (int j = 0; j < NUM_WORKER_THREADS; j++) {
-            sem_post(&worker_start_sem);
-        }
+    *args->shared_buffer_ptr = (struct test_struct*)recv_ptr[0];
+    struct test_struct** local_buffer_ptr = args->shared_buffer_ptr;
 
-        // Wait for workers to complete processing
-        for (int j = 0; j < NUM_WORKER_THREADS; j++) {
-            sem_wait(&worker_done_sem);
-        }
+    // Test jbpf_io_channel_share_data_ptr functionality
+    void* shared_ptr = jbpf_io_channel_share_data_ptr(*local_buffer_ptr);
+    assert(shared_ptr == *local_buffer_ptr);
 
-        // Release the buffer, but shared buffer is still valid
-        jbpf_io_channel_release_buf(current_buffer);
-        current_buffer = NULL;
-    }
+    // Signal the associated worker to start processing
+    sem_post(&worker_start_sem[thread_index]);
+
+    // Release the buffer
+    jbpf_io_channel_release_buf(*local_buffer_ptr);
+
+    // Wait for the worker to complete processing
+    sem_wait(&worker_done_sem[thread_index]);
 
     return NULL;
 }
@@ -104,23 +97,21 @@ consumer_thread_func(void* arg)
 void*
 worker_thread_func(void* arg)
 {
-    while (1) {
-        sem_wait(&worker_start_sem);
+    jbpf_io_register_thread();
+    struct worker_thread_args* args = (struct worker_thread_args*)arg;
+    int thread_index = args->thread_index;
 
-        if (stop_workers) {
-            break;
-        }
+    sem_wait(&worker_start_sem[thread_index]);
+    struct test_struct** local_buffer_ptr = args->shared_buffer_ptr;
 
-        // Process shared buffer
-        struct test_struct* buffer = current_buffer;
-        assert(buffer);
-        uint32_t id = buffer->id;
-        uint32_t value = buffer->value;
-        assert(id * 10 == value);
+    // Process shared buffer
+    struct test_struct* buffer = *local_buffer_ptr;
+    assert(buffer);
+    uint32_t id = buffer->id;
+    uint32_t value = buffer->value;
+    assert(id * 10 == value);
 
-        sem_post(&worker_done_sem);
-    }
-
+    sem_post(&worker_done_sem[thread_index]);
     return NULL;
 }
 
@@ -151,41 +142,50 @@ main(int argc, char* argv[])
 
     assert(local_channel);
 
-    pthread_t producer_thread, consumer_thread, worker_threads[NUM_WORKER_THREADS];
+    pthread_t producer_thread, consumer_threads[NUM_THREADS], worker_threads[NUM_THREADS];
+    struct test_struct* local_current_buffer[NUM_THREADS];
 
     sem_init(&producer_sem, 0, CHANNEL_CAPACITY);
-    sem_init(&consumer_sem, 0, 0);
-    sem_init(&worker_start_sem, 0, 0);
-    sem_init(&worker_done_sem, 0, 0);
-
-    // Start producer and consumer threads
-    assert(pthread_create(&producer_thread, NULL, producer_thread_func, NULL) == 0);
-    assert(pthread_create(&consumer_thread, NULL, consumer_thread_func, NULL) == 0);
-
-    // Start worker threads
-    for (int i = 0; i < NUM_WORKER_THREADS; i++) {
-        assert(pthread_create(&worker_threads[i], NULL, worker_thread_func, NULL) == 0);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        sem_init(&consumer_sem[i], 0, 0);
+        sem_init(&worker_start_sem[i], 0, 0);
+        sem_init(&worker_done_sem[i], 0, 0);
     }
 
-    // Wait for producer and consumer to finish
+    // Start producer thread
+    assert(pthread_create(&producer_thread, NULL, producer_thread_func, NULL) == 0);
+
+    // Start consumer and worker threads
+    struct worker_thread_args worker_args[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; i++) {
+        worker_args[i].shared_buffer_ptr = &local_current_buffer[i];
+        worker_args[i].thread_index = i;
+
+        assert(pthread_create(&consumer_threads[i], NULL, consumer_thread_func, &worker_args[i]) == 0);
+        assert(pthread_create(&worker_threads[i], NULL, worker_thread_func, &worker_args[i]) == 0);
+    }
+
+    // Wait for producer to finish
     assert(pthread_join(producer_thread, NULL) == 0);
-    assert(pthread_join(consumer_thread, NULL) == 0);
 
     // Signal workers to stop
-    stop_workers = 1;
-    for (int i = 0; i < NUM_WORKER_THREADS; i++) {
-        sem_post(&worker_start_sem);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        sem_post(&worker_start_sem[i]);
     }
 
-    for (int i = 0; i < NUM_WORKER_THREADS; i++) {
+    // Wait for consumer and worker threads to finish
+    for (int i = 0; i < NUM_THREADS; i++) {
+        assert(pthread_join(consumer_threads[i], NULL) == 0);
         assert(pthread_join(worker_threads[i], NULL) == 0);
     }
 
     // Cleanup
     sem_destroy(&producer_sem);
-    sem_destroy(&consumer_sem);
-    sem_destroy(&worker_start_sem);
-    sem_destroy(&worker_done_sem);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        sem_destroy(&consumer_sem[i]);
+        sem_destroy(&worker_start_sem[i]);
+        sem_destroy(&worker_done_sem[i]);
+    }
 
     jbpf_io_destroy_channel(io_ctx, local_channel);
     jbpf_io_stop();
