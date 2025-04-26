a5706e4a4699c63565ed8d10879bbf4dac7e6636
Zhihua Lai <zhihualai@microsoft.com>
2025-04-26 12:39:29 +0100

    Comments

--- jbpf_tests/common/jbpf_test_lib.h
+++ jbpf_tests/common/jbpf_test_lib.h
@@ -38,7 +38,7 @@ jbpf_run_test(
     __jbpf_create_test(test_func, setup_func, teardown_func, initial_state, #test_func)
 
 // Macro to assert and avoid unused variable warnings when assert is optimised out
-// in release builds. This is a workaround for the fact that assert is a no-op in
+// in release builds.
 #define __assert__(condition) \
     do { \
         int __assert_result = (int)(condition); \
