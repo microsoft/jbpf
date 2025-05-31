e480314310830c35df30023c72512efe0a668fbb
Xenofon Foukas <xefouk@microsoft.com>
2025-05-31 23:54:17 +0100

    Updated prevail patch file

--- init_and_patch_submodules.sh
+++ init_and_patch_submodules.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -rf 3p/ebpf-verifier 3p/mimalloc 3p/ck 3p/ubpf
+rm -rf 3p/prevail 3p/mimalloc 3p/ck 3p/ubpf
 git submodule update --init --recursive
 
 cp patches/ck.patch 3p/ck
 cp patches/mimalloc.patch 3p/mimalloc
-cp patches/ebpf_verifier.patch 3p/ebpf-verifier/
+cp patches/prevail.patch 3p/prevail/
 
 cd 3p/ck
 git apply ck.patch
@@ -13,5 +13,5 @@ git apply ck.patch
 cd ../mimalloc
 git apply mimalloc.patch
 
-cd ../ebpf-verifier/
-git apply ebpf_verifier.patch
+cd ../prevail/
+git apply prevail.patch
