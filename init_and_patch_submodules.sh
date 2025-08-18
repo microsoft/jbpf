e410c9cac3e4ab323f5e7474f7470debd8b862f9
Bozidar Radunovic <bozidar@microsoft.com>
2025-08-18 11:03:33 +0000

    This is a temporary patch to fix missing BoostConfig.cmake file from AZL3 Boost package See: https://github.com/vbpf/prevail/commit/803b647acbfd5985746b664da5d751161dd564ac

--- init_and_patch_submodules.sh
+++ init_and_patch_submodules.sh
@@ -6,6 +6,9 @@ git submodule update --init --recursive
 cp patches/ck.patch 3p/ck
 cp patches/mimalloc.patch 3p/mimalloc
 cp patches/prevail.patch 3p/prevail/
+# This is a temporary patch to fix missing BoostConfig.cmake file from AZL3 Boost package
+# See: https://github.com/vbpf/prevail/commit/803b647acbfd5985746b664da5d751161dd564ac
+cp patches/prevail_cmake.patch 3p/prevail/
 
 cd 3p/ck
 git apply ck.patch
@@ -15,3 +18,4 @@ git apply mimalloc.patch
 
 cd ../prevail/
 git apply prevail.patch
+git apply prevail_cmake.patch
